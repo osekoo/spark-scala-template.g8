8d9b6a3cbc365c027d37a612d2152959dd744313
wopoadi <kodjo@osekoo.com>
2023-10-26 21:06:17 +0200

    Add scala, jdk and java to the properties

---
+++ src/main/g8/run.bat
@@ -0,0 +1,3 @@
+docker compose down -v
+docker compose up spark-worker -d
+docker compose up $name;format="normalize"$-spark-client
---
+++ src/main/g8/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker compose down -v
+docker compose up spark-worker -d
+docker compose up $name;format="normalize"$-spark-client
