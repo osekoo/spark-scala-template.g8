50617fa1f2a91948d4743641807e92a790d8fe84
wopoadi <kodjo@osekoo.com>
2023-10-26 19:50:23 +0200

    Add scala, jdk and java to the properties

--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -1,5 +1,31 @@
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Main class of `$name$`")
+    println("Running `$name$`...")
+/*
+    // Initializing Spark session
+ var masterUrl = System.getenv("SPARK_MASTER_URL")
+    if (masterUrl == null) {
+      masterUrl = "local[*]"
+    }
+    println(s"master url: $masterUrl")
+    val spark: SparkSession = SparkSession.builder()
+      .appName(s"Hello Spark")
+      .master(masterUrl)
+      .getOrCreate()
+
+    val columns = Seq("name", "age")
+    val data = Seq(("Tom", "20"),
+      ("Job", "99"),
+      ("Eva", "43"))
+    val rdd = spark.sparkContext.parallelize(data)
+    val df = spark.createDataFrame(rdd)
+      .toDF(columns: _*)
+    df.printSchema()
+    df.show()
+
+    // Saving people's name into names.csv
+    df.select("name").write
+      .mode(SaveMode.Overwrite)
+      .parquet("names.parquet")*/
   }
 }
